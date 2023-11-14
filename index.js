f26559bde3d5c83850a880a34c886c3258fe739c
Charles Danielle Paglingayen <charlesdanius@gmail.com>
2023-11-14 21:52:52 +0800

    Added html and js files with basic functions needed

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('External JavaScript is working')
+
+const getComputerChoice = () => {}
+
+const playRound = (playerSelection, computerSelection) => {}
+
+const playerSelection = 'rock'
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
+
+const game = () => {}
