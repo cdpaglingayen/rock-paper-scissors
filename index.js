09469810da306b4fbe8454d3c54f9432fb4c0a53
Charles Danielle Paglingayen <charlesdanius@gmail.com>
2024-02-07 18:44:59 +0800

    Restarting the file content after long time

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-console.log('External JavaScript is working')
-
-const getComputerChoice = () => {}
-
-const playRound = (playerSelection, computerSelection) => {}
-
-const playerSelection = 'rock'
-const computerSelection = getComputerChoice()
-console.log(playRound(playerSelection, computerSelection))
-
-const game = () => {}
+console.log('Hello World from External File')
